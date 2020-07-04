0c3606bd9d66593c09c6ef5a0121c854c833e218
WEN Hao <wenh06@gmail.com>
2020-07-04 14:30:56 +0800

    update utility functions

--- misc.py
+++ misc.py
@@ -1,7 +1,9 @@
 """
 """
 from numbers import Real
-from typing import Union, Optional, List, Tuple
+from typing import Union, Optional, List, Tuple, NoReturn
+
+import numpy as np
 
 
 __all__ = [
@@ -196,13 +198,68 @@ def dict_to_str(d:Union[dict, list, tuple], current_depth:int=1, indent_spaces:i
             if isinstance(v, (dict, list, tuple)):
                 s += f"{prefix}{dict_to_str(v, current_depth+1)}\n"
             else:
-                s += f"{prefix}{v}\n"
+                val = f'\042{v}\042' if isinstance(v, str) else v
+                s += f"{prefix}{val}\n"
     elif isinstance(d, dict):
         for k, v in d.items():
             if isinstance(v, (dict, list, tuple)):
                 s += f"{prefix}{k}: {dict_to_str(v, current_depth+1)}\n"
             else:
-                s += f"{prefix}{k}: {v}\n"
+                key = f'\042{k}\042' if isinstance(k, str) else k
+                val = f'\042{v}\042' if isinstance(v, str) else v
+                s += f"{prefix}{key}: {val}\n"
     s += unit_indent*(current_depth-1)
     s = f"{{{s}}}" if isinstance(d, dict) else f"[{s}]"
     return s
+
+
+def plot_single_lead_ecg(s:np.ndarray, fs:Real, use_idx:bool=False, **kwargs) -> NoReturn:
+    """ not finished
+
+    single lead ECG plot,
+
+    Parameters:
+    -----------
+    s: array_like,
+        the single lead ECG signal
+    fs: real,
+        sampling frequency of `s`
+    use_idx: bool, default False,
+        use idx instead of time for the x-axis
+
+    contributors: Jeethan, and WEN Hao
+    """
+    if 'plt' not in dir():
+        import matplotlib.pyplot as plt
+    default_fig_sz = 120
+    line_len = fs * 25  # 25 seconds
+    nb_lines, residue = divmod(len(s), line_len)
+    if residue > 0:
+        nb_lines += 1
+    for idx in range(nb_lines):
+        idx_start = idx*line_len
+        idx_end = min((idx+1)*line_len, len(s))
+        c = s[idx_start:idx_end]
+        secs = np.arange(idx_start, idx_end)
+        if not use_idx:
+            secs = secs / fs
+        mvs = np.array(c) * 0.001
+        fig_sz = int(round(default_fig_sz * (idx_end-idx_start)/line_len))
+        fig, ax = plt.subplots(figsize=(fig_sz, 6))
+        ax.plot(secs, mvs, c='black')
+
+        ax.axhline(y=0, linestyle='-', linewidth='1.0', color='red')
+        ax.xaxis.set_major_locator(plt.MultipleLocator(0.2))
+        ax.xaxis.set_minor_locator(plt.MultipleLocator(0.04))
+        ax.yaxis.set_major_locator(plt.MultipleLocator(0.5))
+        ax.yaxis.set_minor_locator(plt.MultipleLocator(0.1))
+        ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+        ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+        ax.set_xlim(secs[0], secs[-1])
+        ax.set_ylim(-1.5, 1.5)
+        if use_idx:
+            plt.xlabel('Samples')
+        else:
+            plt.xlabel('Time [s]')
+        plt.ylabel('Voltage [mV]')
+        plt.show()
