6c7bd25a2648adb11e1b76cbbf902be7b62520c6
WEN Hao <wenh06@gmail.com>
2020-07-06 00:31:23 +0800

    init construction of dl models

--- models/load_model.py
+++ models/load_model.py
@@ -7,11 +7,11 @@ from cfg import TrainCfg
 __all__ = ["load_model"]
 
 
-def load_model():
+def load_model(field:str='ml'):
     """
     """
     try:
-        model = joblib.load(TrainCfg.model_path)
+        model = joblib.load(TrainCfg.model_path[field.lower()])
     except:
         model = None
     return model
--- models/train_model_dl.py
+++ models/train_model_dl.py
@@ -1,10 +1,27 @@
 """
 """
 import argparse
-import joblib, pickle
 from copy import deepcopy
-import xgboost as xgb
-from sklearn.model_selection import GridSearchCV
+
+from keras import layers
+from keras import Input
+from keras.models import Sequential, Model, load_model
+from keras.layers import (
+    Layer,
+    LSTM, GRU,
+    TimeDistributed, Bidirectional,
+    ReLU, LeakyReLU,
+    BatchNormalization,
+    Dense, Dropout, Activation, Flatten, 
+    Input, Reshape, GRU, CuDNNGRU,
+    Conv1D,
+    MaxPooling1D,
+    GlobalAveragePooling1D, AveragePooling1D,
+    concatenate, add,
+)
+from keras.initializers import he_normal, he_uniform, Orthogonal
+from keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, CSVLogger
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from easydict import EasyDict as ED
 
@@ -13,7 +30,56 @@ from signal_processing.ecg_preprocess import parallel_preprocess_signal
 from .data_generator import CPSC2020
 
 
-__all__ = ["train"]
+__all__ = [
+    "train",
+    "TI_CNN", "ATI_CNN",
+]
+
+
+class TI_CNN(Sequential):
+    """
+    """
+    def __init__(self, classes:list, input_len:int, cnn:str, bidirectional:bool=True):
+        """
+        """
+        super(Sequential, self).__init__(name='TI_CNN')
+        self.classes = classes
+        self.nb_classes = len(classes)
+        self.input_len = input_len
+        self.bidirectional = bidirectional
+        self.cnn = cnn.lower()
+
+        if self.cnn == 'vgg':
+            self._build_vgg_model()
+        elif self.cnn == 'resnet':
+            self._build_resnet_model()
+        elif self.cnn == 'xception':
+            self._build_xception_model()
+
+    def _build_vgg_model(self):
+        """
+        """
+        raise NotImplementedError
+
+    def _build_resnet_model(self):
+        """
+        """
+        raise NotImplementedError
+
+
+class ATI_CNN(TI_CNN):
+    """
+    """
+    def __init__(self, classes:list, input_len:int, cnn:str, bidirectional:bool=True):
+        """
+        """
+        super(Sequential, self).__init__(name='TI_CNN')
+        self.classes = classes
+        self.nb_classes = len(classes)
+        self.input_len = input_len
+        self.bidirectional = bidirectional
+        self.cnn = cnn.lower()
+
 
 
 def train(**config):
