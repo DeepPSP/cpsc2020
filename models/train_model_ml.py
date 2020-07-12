2491e35b80d5d3731f540967be738c8cba52e5ce
WEN Hao <wenh06@gmail.com>
2020-07-12 17:27:07 +0800

    resolve DAS training ModuleNotFoundError

--- models/train_model_ml.py
+++ models/train_model_ml.py
@@ -16,8 +16,10 @@ TODO:
        with reference to the official scoring function,
        which lay more punishment on false negatives (5 times)
 """
-import sys
-import os
+import os, sys
+# for DAS training ModuleNotFoundError:
+_PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, _PARENT_DIR)
 import argparse
 import joblib, pickle
 import time, datetime
@@ -326,10 +328,6 @@ if __name__ == "__main__":
 
     config = deepcopy(TrainCfg)
     # config.update(kw)
-
-    # for DAS training ModuleNotFoundError:
-    _PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(0, _PARENT_DIR)
 
     for m in models:
         trainer = ECGPrematureDetector(
