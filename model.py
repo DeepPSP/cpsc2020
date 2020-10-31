54c1d1d899e1f55459fb39d5b1bf227c49ce70ed
WEN Hao <wenh06@gmail.com>
2020-10-31 20:55:20 +0800

    add ECG_SEQ_LAB_NET_CPSC2020

--- model.py
+++ model.py
@@ -10,11 +10,13 @@ from torch import Tensor
 from easydict import EasyDict as ED
 
 from torch_ecg.torch_ecg.models.ecg_crnn import ECG_CRNN
+from torch_ecg.torch_ecg.models.ecg_seq_lab_net import ECG_SEQ_LAB_NET
 from cfg import ModelCfg
 
 
 __all__ = [
     "ECG_CRNN_CPSC2020",
+    "ECG_SEQ_LAB_NET_CPSC2020",
 ]
 
 
@@ -40,7 +42,7 @@ class ECG_CRNN_CPSC2020(ECG_CRNN):
             other hyper-parameters, including kernel sizes, etc.
             ref. the corresponding config file
         """
-        model_config = deepcopy(ModelCfg)
+        model_config = deepcopy(ModelCfg.crnn)
         model_config.update(config or {})
         super().__init__(classes, n_leads, input_len, model_config)
 
@@ -92,7 +94,67 @@ class ECG_CRNN_CPSC2020(ECG_CRNN):
                     np.array(self.classes)[np.where(bin_pred==1)[0]].tolist()
         return pred, bin_pred
 
-    def inference_CINC2020(self):
+    def inference_CPSC2020(self, input:Union[np.ndarray,Tensor], class_names:bool=False, bin_pred_thr:float=0.5) -> Tuple[Union[np.ndarray, pd.DataFrame], np.ndarray]:
         """
+        alias for `self.inference`
         """
-        raise NotImplementedError("method for CINC2020 should not be used in CPSC2020")
+        return self.inference(input, class_names, bin_pred_thr)
+
+
+
+class ECG_SEQ_LAB_NET_CPSC2020(ECG_SEQ_LAB_NET):
+    """
+    """
+    __DEBUG__ = True
+    __name__ = "ECG_SEQ_LAB_NET_CPSC2020"
+
+    def __init__(self, classes:Sequence[str], n_leads:int, input_len:Optional[int]=None, config:Optional[ED]=None) -> NoReturn:
+        """ finished, checked,
+
+        Parameters:
+        -----------
+        classes: list,
+            list of the classes for sequence labeling
+        n_leads: int,
+            number of leads (number of input channels)
+        input_len: int, optional,
+            sequence length (last dim.) of the input,
+            will not be used in the inference mode
+        config: dict, optional,
+            other hyper-parameters, including kernel sizes, etc.
+            ref. the corresponding config file
+        """
+        model_config = deepcopy(ModelCfg.seq_lab)
+        model_config.update(config or {})
+        super().__init__(classes, n_leads, input_len, model_config)
+
+    @torch.no_grad()
+    def inference(self, input:Union[np.ndarray,Tensor], class_names:bool=False, bin_pred_thr:float=0.5) -> Tuple[Union[np.ndarray, pd.DataFrame], np.ndarray]:
+        """ finished, checked,
+
+        auxiliary function to `forward`, for CPSC2020,
+
+        Parameters:
+        -----------
+        input: ndarray or Tensor,
+            input tensor, of shape (batch_size, channels, seq_len)
+        class_names: bool, default False,
+            if True, the returned scalar predictions will be a `DataFrame`,
+            with class names for each scalar prediction
+        bin_pred_thr: float, default 0.5,
+            the threshold for making binary predictions from scalar predictions
+
+        Returns:
+        --------
+        pred: ndarray or DataFrame,
+            scalar predictions, (and binary predictions if `class_names` is True)
+        bin_pred: ndarray,
+            the array (with values 0, 1 for each class) of binary prediction
+        """
+        raise NotImplementedError
+
+    def inference_CPSC2020(self, input:Union[np.ndarray,Tensor], class_names:bool=False, bin_pred_thr:float=0.5) -> Tuple[Union[np.ndarray, pd.DataFrame], np.ndarray]:
+        """
+        alias for `self.inference`
+        """
+        return self.inference(input, class_names, bin_pred_thr)
